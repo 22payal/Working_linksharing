9a7d6fec138cfbb6d669a1dd0afd59f8f51d8161
22payal <payalnigam1996@gmail.com>
2018-03-27 21:23:59 +0530

    initial commit

---
+++ grails-app/controllers/newpackage/UtilController.groovy
@@ -0,0 +1,17 @@
+package newpackage
+
+class UtilController {
+
+    def index() {
+
+        log.trace "hello world"
+        log.error("error found")
+        log.warn("warning started")
+        log.info("info stated")
+        log.debug("debugging started")
+
+        log.info("request params: $params")
+
+        render("hello user ")
+    }
+}
