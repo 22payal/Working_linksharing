9cc3d3f253728d91633619071e8658f20d83bc1c
22payal <payalnigam1996@gmail.com>
2018-04-01 13:19:45 +0530

    ques5

--- grails-app/domain/linksharing/Resource.groovy
+++ grails-app/domain/linksharing/Resource.groovy
@@ -2,6 +2,7 @@ package linksharing
 
 import co.ResourceSearchCo
 import vo.RatingInfoVo
+import vo.ResourceVo
 
 
 abstract class Resource {
@@ -78,6 +79,22 @@ abstract class Resource {
                 return ratingInfoVo
 
     }
+
+    static List<ResourceVo> getTopPost(){
+             List<ResourceVo> topPosts = ResourceRating.createCriteria().list(){
+                        projections{
+                                createAlias('resource', 'r')
+                                groupProperty('r.id')
+                                property('r.createdBy')
+                                property('r.topic.name')
+                                count('r.id', 'count')
+                            }
+                        order('count', 'desc')
+                        maxResults(5)
+                    }
+               return topPosts
+            }
+
 
 
 }
---
+++ src/main/groovy/vo/ResourceVo.groovy
@@ -0,0 +1,9 @@
+package vo
+
+class ResourceVo {
+    Integer id
+    Integer count
+    String createdBy
+    String topicName
+
+}
