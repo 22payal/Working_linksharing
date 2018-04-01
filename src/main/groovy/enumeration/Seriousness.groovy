71ff8c788af69b0af2745813c5fc3fa63225b808
22payal <payalnigam1996@gmail.com>
2018-04-01 11:41:26 +0530

    ques14-18

--- src/main/groovy/enumeration/Seriousness.groovy
+++ src/main/groovy/enumeration/Seriousness.groovy
@@ -3,5 +3,11 @@ package enumeration
 enum Seriousness {
 
     SERIOUS, VERYSERIOUS, CASUAL
+    static Seriousness convertSeriousness(String inputString) {
+        inputString = inputString.toUpperCase()
+        Seriousness seriousness = inputString
+
+        return seriousness
+    }
 
 }
