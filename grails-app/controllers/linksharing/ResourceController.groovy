77c9c8fec7cc0e7130e23fcb2759a6c9a64f60ec
22payal <payalnigam1996@gmail.com>
2018-04-02 08:29:07 +0530

    views updated

--- grails-app/controllers/linksharing/ResourceController.groovy
+++ grails-app/controllers/linksharing/ResourceController.groovy
@@ -1,53 +1,54 @@
-//import linksharing.Resource
-//import linksharing.Topic
-//import vo.TopicVo
-//
-//import co.ResourceSearchCo
-//import enumeration.Visibility
-//import vo.RatingInfoVo
-//import vo.TopicVo
-//
-//class ResourceController {
-//
-//    def index() { }
-//
-//    def resourceDelete(Integer id)
-//    {
-//        Resource resource=Resource.load(id)
-//
-//        if(!resource)
-//        {
-//            throw new Exception( "exception of object not found")
-//        }
-//    }
-//
-//    def search(){
-//        ResourceSearchCo resourceSearchCo=new ResourceSearchCo()
-//        if(resourceSearchCo.q)
-//            resourceSearchCo.visibility=Visibility.PUBLIC
-//
-//    }
-//
-//    def show(Integer ResourceId)
-//    {
-//        Resource resource = Resource.findById(resourceId)
-//               RatingInfoVo ratingInfoVo = resource.getResourceRatingInformation()
-//                render("TOTAL VOTES- $ratingInfoVo.totalVotes + TOTAL SCORE- $ratingInfoVo.totalScore + AVERAGE SCORE- $ratingInfoVo.averageScore")
-//
-//        List<TopicVo> trendingTopics = Topic.getTrendingTopics()
-//        render("TRENDING TOPICS-"+trendingTopics.each {println("$it.name + $it.visibility + $it.createdBy")})
-//
-//    }
-//
-//    def handleNullPointerException(NullPointerException e) {
-//
-//        render ("null found")
-//    }
-//
-//def findTrendingTopics(){
-//            List<TopicVo> trendingTopics = Topic.getTrendingTopics()
-//        render("TRENDING TOPICS-" +
-//            trendingTopics.each {println("$it.name + $it.visibility + $it.createdBy")})
-//}
-//
-//}
+package linksharing
+
+import co.ResourceSearchCo
+import enumeration.Visibility
+import linksharing.Resource
+import vo.RatingInfoVo
+import vo.TopicVo
+import linksharing.Topic
+
+
+class ResourceController {
+
+    def index() { }
+
+    def resourceDelete(Integer id)
+    {
+        Resource resource=Resource.load(id)
+
+        if(!resource)
+        {
+            throw new Exception( "exception of object not found")
+        }
+    }
+
+    def search(){
+        ResourceSearchCo resourceSearchCo=new ResourceSearchCo()
+        if(resourceSearchCo.q)
+            resourceSearchCo.visibility=Visibility.PUBLIC
+
+    }
+
+    def show(Integer ResourceId)
+    {
+        Resource resource = Resource.findById(resourceId)
+               RatingInfoVo ratingInfoVo = resource.getResourceRatingInformation()
+                render("TOTAL VOTES- $ratingInfoVo.totalVotes + TOTAL SCORE- $ratingInfoVo.totalScore + AVERAGE SCORE- $ratingInfoVo.averageScore")
+
+        List<TopicVo> trendingTopics = Topic.getTrendingTopics()
+        render("TRENDING TOPICS-"+trendingTopics.each {println("$it.name + $it.visibility + $it.createdBy")})
+
+    }
+
+    def handleNullPointerException(NullPointerException e) {
+
+        render ("null found")
+    }
+
+def findTrendingTopics(){
+            List<TopicVo> trendingTopics = Topic.getTrendingTopics()
+        render("TRENDING TOPICS-" +
+            trendingTopics.each {println("$it.name + $it.visibility + $it.createdBy")})
+}
+
+}
