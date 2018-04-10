91881057f501bc683e9f83717d68c70896584f6c
22payal <payalnigam1996@gmail.com>
2018-04-10 13:49:52 +0530

    daily job updated

--- grails-app/jobs/linksharing/EmailsenderJob.groovy
+++ grails-app/jobs/linksharing/EmailsenderJob.groovy
@@ -13,6 +13,7 @@ class EmailsenderJob {
 //         simple repeatInterval: 5000l // execute job once in 5 seconds
 //      simple name: 'simpleTrigger', startDelay: 10000, repeatInterval: 30000, repeatCount: 10
         cron name: 'cronTrigger', startDelay: 10000, cronExpression: '0 0 1 ? * MON *'
+        cron name: 'cronTrigger', startDelay: 10000, cronExpression:'0 0 12 1/1 * ? *'
 //        custom name: 'customTrigger', triggerClass: MyTriggerClass, myParam: myValue, myAnotherParam: myAnotherValue
     }
 
